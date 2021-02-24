da3ae6fc0baab49dbe1df2d355ea21368c5fb7c0
anighanta <anirudh.ghanta@hedera.com>
2021-02-24 17:29:37 -0600

    test 22word vectors from android
    Signed-off-by: anighanta <anirudh.ghanta@hedera.com>

--- IntegrationTests/EDKeyPairTests.swift
+++ IntegrationTests/EDKeyPairTests.swift
@@ -61,11 +61,19 @@ class EDKeyPairTests: XCTestCase {
     func testWordListFromAndroid() {
         let wordStr1 = "cheese outside tilt amateur curtain manage renew emotion torch album dune easy chaos balcony sort twenty involve other echo age blame urge famous avocado"
         
-        let words = wordStr1.trim().components(separatedBy: CharacterSet.init(charactersIn: "\n\t "))
-        let seed:HGCSeed  = HGCSeed.init(bip39Words: words)
+        let wordStr2 = "nut keep pulse maxim sway fled dog decor astute serve repeal opaque wily lied mosaic slide sour towel core chaos blood insult"
         
-        let keyPair = EDBip32KeyChain.init(hgcSeed: seed).key(at: 0)
-        XCTAssertEqual("99ebc34ab219e8fb1bb14c813803e0a6ebde2c69ca2d729da0e8f9186749648f", keyPair?.publicKeyData.hex)
-        XCTAssertEqual("002a26eafcf673fc2d7ccd5d2091fc63993f9548db579677f3e27d5813e63d7499ebc34ab219e8fb1bb14c813803e0a6ebde2c69ca2d729da0e8f9186749648f", keyPair?.privateKeyData.hex)
+        let words24 = wordStr1.trim().components(separatedBy: CharacterSet.init(charactersIn: "\n\t "))
+        let words22 = wordStr2.trim().components(separatedBy: CharacterSet.init(charactersIn: "\n\t "))
+        let bip32seed:HGCSeed  = HGCSeed.init(bip39Words: words24)
+        let preBip32seed:HGCSeed  = HGCSeed.init(words: words22)
+        
+        let bip32keyPair = EDBip32KeyChain.init(hgcSeed: bip32seed).key(at: 0)
+        XCTAssertEqual("99ebc34ab219e8fb1bb14c813803e0a6ebde2c69ca2d729da0e8f9186749648f", bip32keyPair?.publicKeyData.hex)
+        XCTAssertEqual("002a26eafcf673fc2d7ccd5d2091fc63993f9548db579677f3e27d5813e63d7499ebc34ab219e8fb1bb14c813803e0a6ebde2c69ca2d729da0e8f9186749648f", bip32keyPair?.privateKeyData.hex)
+        
+        let preBIP32KeyPair = EDKeyChain.init(hgcSeed: preBip32seed).key(at: 0)
+        XCTAssertEqual("485de28538c5f6b627fb5a3dea1bf2386089b79325d6d65a7cd366ef4730d4d8", preBIP32KeyPair?.publicKeyData.hex)
+        XCTAssertEqual("819c0f7a3251ad2fe294591c3f89849b4a2e0ab311e32c03f5b29f62dd5af2a8485de28538c5f6b627fb5a3dea1bf2386089b79325d6d65a7cd366ef4730d4d8", preBIP32KeyPair?.privateKeyData.hex)
     }
 }
